8a2dda84ce90f5090f6d8898663de8b92bd4c02b
smart2538 <worapon.o@ku.th>
2015-11-15 03:47:14 +0700

    add nuttrition dummy call

--- app/controllers/Schedule.java
+++ app/controllers/Schedule.java
@@ -9,12 +9,14 @@ import views.html.workout_schedule;
 import org.easyrules.api.RulesEngine;
 import static org.easyrules.core.RulesEngineBuilder.aNewRulesEngine;
 import rules.exercise.Day;
+import rules.nutrition.*;
 import rules.*;
 /**
  * Created by Frank on 11/14/2015 AD.
  */
 public class Schedule extends Controller {
     private static ExerciseScheduleRule exSchRule = new ExerciseScheduleRule();
+    private static NutritionRule nuRule = new NutritionRule();
 
     public static Result index() {
         return ok(select_schedule.render());
@@ -33,7 +35,15 @@ public class Schedule extends Controller {
     }
 
     public static Result nutritionSchedule() {
-        return ok(nutrition_schedule.render());
+        double weight = 80;
+        boolean isGain = false;
+        nuRule.setInput(weight,isGain);
+        RulesEngine rulesEngine = aNewRulesEngine().withSilentMode(true).build();
+        rulesEngine.registerRule(nuRule);
+        rulesEngine.fireRules();
+        List<Nutrition> nuList = nuRule.getResult();
+
+        return ok(nutrition_schedule.render(nuList));
     }
 
 }
