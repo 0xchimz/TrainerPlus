d9e851a631073e59d5751e8cef7160c977709ed2
Sorrawit Chancherngkit <ongoing@outlook.com>
2015-11-14 22:20:03 +0700

    fix exerciseGenerator bugs

--- app/rules/CalEatingRule.java
+++ app/rules/CalEatingRule.java
@@ -4,36 +4,38 @@ import org.easyrules.annotation.Action;
 import org.easyrules.annotation.Condition;
 import org.easyrules.annotation.Rule;
 
-@Rule(name = "Weight Management rule", description = "to gain or to lose.")
-public class WeightManagementRule {
+@Rule(name = "Calories Eating rule", description = "to gain or to lose.")
+public class CalEatingRule {
 
     /**
      * The user input which represents the data that the rule will operate on.
      */
+    private double TDEE;
     private String goal;
     private String result;
 
     @Condition
     public boolean checkInput() {
         //The rule should be applied only if the user's response is yes (duke friend)
-        return (goal.equalsIgnoreCase("gain") | goal.equalsIgnoreCase("lose"));
+        return (goal.equalsIgnoreCase("gain") | goal.equalsIgnoreCase("lose")) & TDEE > 0;
     }
 
     @Action(order = 1)
     public void checkGain() throws Exception {
         if (goal.equalsIgnoreCase("gain")){
-        	result = String.format("You need to workout");
+        	result = String.format("You need to eat more than %d kcal\n",TDEE);
         }
     }
     
     @Action(order = 2)
     public void checkLose() throws Exception {
         if (goal.equalsIgnoreCase("lose")){
-        	result = String.format("You need to cardio and Workout");
+        	result = String.format("You need to eat less than %d kcal\n",TDEE);
         }
     }
 
-    public void setInput(String goal) {
+    public void setInput(double TDEE, String goal) {
+        this.TDEE = TDEE;
         this.goal = goal;
     }
     public String getResult() {
