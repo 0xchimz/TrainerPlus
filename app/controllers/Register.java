e57dd5b275835eba526cba140e12c19f03376517
ChinCluBi <chincub@gmail.com>
2015-11-15 23:56:43 +0700

    Add register router

--- app/controllers/Register.java
+++ app/controllers/Register.java
@@ -2,12 +2,13 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.regis;
 /**
  * Created by Frank on 11/15/2015 AD.
  */
 public class Register extends Controller {
 
     public static Result register() {
-        return ok(views.html.regis.render());
+        return ok(regis.render());
     }
 }
