db901b23d2f1d30d0565092bb4c5ad8f933ab27a
ChinCluBi <chincub@gmail.com>
2015-11-16 01:13:11 +0700

    Register submit

--- app/controllers/Register.java
+++ app/controllers/Register.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import play.data.DynamicForm;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.regis;
@@ -10,5 +11,12 @@ public class Register extends Controller {
 
     public static Result register() {
         return ok(regis.render());
+    }
+
+    public static Result submit() {
+        DynamicForm dynamicForm = new DynamicForm().bindFromRequest();
+//        String email = dynamicForm.get("email");
+//        String password = dynamicForm.get("password");
+//        String
     }
 }
