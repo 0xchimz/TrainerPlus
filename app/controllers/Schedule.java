ffad98a88a2db36bf4169822bb1f5b890dad9736
smart2538 <worapon.o@ku.th>
2015-11-15 20:35:57 +0700

    cardio page is working now

--- app/controllers/Schedule.java
+++ app/controllers/Schedule.java
@@ -10,28 +10,35 @@ import org.easyrules.api.RulesEngine;
 import static org.easyrules.core.RulesEngineBuilder.aNewRulesEngine;
 import rules.exercise.Day;
 import rules.nutrition.*;
+import rules.cardio.*;
 import rules.*;
+
 /**
  * Created by Frank on 11/14/2015 AD.
  */
 public class Schedule extends Controller {
     private static ExerciseScheduleRule exSchRule = new ExerciseScheduleRule();
     private static NutritionRule nuRule = new NutritionRule();
-
+    private static CardioRule caRule = new CardioRule();
     public static Result index() {
         return ok(select_schedule.render());
     }
 
     public static Result workoutSchedule() {
         int userWorkoutDays = 3;
-        boolean isIntense = true;
+        int age = 20;
+        boolean workoutIsIntense = true;
+        boolean cardioIsIntense = false;
         String gender = "male";
-        exSchRule.setInput(userWorkoutDays, isIntense, gender );
+        exSchRule.setInput(userWorkoutDays, workoutIsIntense, gender );
+        caRule.setInput(age, cardioIsIntense );
         RulesEngine rulesEngine = aNewRulesEngine().withSilentMode(true).build();
         rulesEngine.registerRule(exSchRule);
+        rulesEngine.registerRule(caRule);
         rulesEngine.fireRules();
-        List<Day> scheduleList = exSchRule.getResult();
-        return ok(workout_schedule.render(scheduleList));
+        List<Day> workoutScheduleList = exSchRule.getResult();
+        List<Cardio> cardiotScheduleList = caRule.getResult();
+        return ok(workout_schedule.render(workoutScheduleList, cardiotScheduleList, cardioIsIntense));
     }
 
     public static Result nutritionSchedule() {
