f7135d0e9e5442d2ad89df51c9c0aa1883756434
smart2538 <worapon.o@ku.th>
2015-11-16 00:26:39 +0700

    fix bug

--- app/controllers/Home.java
+++ app/controllers/Home.java
@@ -14,10 +14,6 @@ public class Home extends Controller {
     public static Result index() {
         return ok(home.render());
     }
-    public static Result editProfile(){
-        return ok(profile_information.render());
-    }
-
     public static Result mock() {
         User.create("chincub@gmail.com","015335510");
         return ok(home.render());
--- app/controllers/Profile.java
+++ app/controllers/Profile.java
@@ -3,7 +3,7 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.profile;
-import play.data.*
+import play.data.*;
 import models.*;
 
 /**
@@ -18,14 +18,12 @@ public class Profile extends Controller {
     public static Result editProfile(){
       DynamicForm dynamicForm = new DynamicForm().bindFromRequest();
       User thisUser = User.findById(Long.parseLong(session("userId")));
-      thisUser.setHeight(dynamicForm.get("input_height"));
-      thisUser.setWeight(dynamicForm.get("input_weight"));
-      thisUser.setWaistline(dynamicForm.get("input_waistline"));
-      thisUser.setHip(dynamicForm.get("input_hip"));
-      thisUser.setGender(dynamicForm.get("input_gender"));
-      thisUser.setIsGain(dynamicForm.get("input_goal"));
+      thisUser.setHeight(Double.parseDouble(dynamicForm.get("input_height")));
+      thisUser.setWeight(Double.parseDouble(dynamicForm.get("input_weight")));
+      thisUser.setWaistline(Double.parseDouble(dynamicForm.get("input_waistline")));
+      thisUser.setHip(Double.parseDouble(dynamicForm.get("input_hip")));
+      thisUser.setIsGain(Boolean.parseBoolean(dynamicForm.get("input_goal")));
       thisUser.update();
-      String var = null;
       return ok(profile.render());
 
     }
